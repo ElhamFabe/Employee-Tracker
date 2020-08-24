20e601a9bc66eed24988f663edeb84276b967f55
Elham Fabe <elhamfabe@gmail.com>
2020-08-24 18:03:49 -0400

    env file

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-const conTable = require ("console.table")
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+const conTable = require ('console.table');
+const Denv = require('dotenv').config();
+
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
+const db = require('db')
 
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "greatBay_DB"
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
 });
